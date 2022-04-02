034ef5f6ffa8be4c5abf0b03a3e5e579abc3b3ce
Geniroh <irochibuzor@gmail.com>
2022-04-02 01:51:50 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int myFavNum = 7;
+            double myDoubleNum = 5.990;
+            char myFirstLetter = 'C';
+            bool myBool = true;
+            string myName = "Chibuzor";
+            float myPi = 10.2f;
+            decimal amount = 100.50M;
+            dynamic myText = "The boss";
+
+            //IMPLICIT TYPE CONVERSION
+            double myPi2 = myPi;
+
+            //EXPLICIT TYPE CONVERSION
+            int myamount = (int) amount;
+
+            Console.WriteLine(myFavNum);
+            Console.WriteLine(myDoubleNum);
+            Console.WriteLine(myBool);
+            Console.WriteLine(amount);
+            Console.WriteLine(myPi);
+            Console.WriteLine(myName);
+            Console.WriteLine(myText);
+            Console.WriteLine(myFirstLetter);
+            Console.WriteLine(myPi2);
+            Console.WriteLine(myamount);
+        }
+    }
+}
